716287dca3822fef4e6b4ae7ee66eaa8245f2843
kimhg93 <kimhg93@gmail.com>
2022-09-22 18:12:39 +0900

    programmers level2 12914, 12945, 12973

---
+++ src/programmers/learn/challenges/level2/Lessons12945.java
@@ -0,0 +1,59 @@
+package programmers.learn.challenges.level2;
+
+/**
+ * 연습문제
+ * 피보나치 수
+ */
+public class Lessons12945 {
+
+    public static void main(String[] args) throws Exception {
+        int[] arr = {0};
+        String[] arr2 = {""};
+        int n = 0;
+        String str = "";
+
+        long s = System.currentTimeMillis();
+        System.err.println(solution(6));
+        long e = System.currentTimeMillis();
+        System.err.println((e-s)+ "ms");
+    }
+
+    public static int solution(int n) {
+        int a = 1;
+        int b = 1;
+        int tmp = 0;
+        for(int i=2; i<=n; i++){
+            tmp = (a+b)%1234567;
+            a = b;
+            b = tmp;
+        }
+        return a;
+    }
+}
+
+/*
+피보나치 수
+문제 설명
+피보나치 수는 F(0) = 0, F(1) = 1일 때, 1 이상의 n에 대하여 F(n) = F(n-1) + F(n-2) 가 적용되는 수 입니다.
+
+예를들어
+
+F(2) = F(0) + F(1) = 0 + 1 = 1
+F(3) = F(1) + F(2) = 1 + 1 = 2
+F(4) = F(2) + F(3) = 1 + 2 = 3
+F(5) = F(3) + F(4) = 2 + 3 = 5
+와 같이 이어집니다.
+
+2 이상의 n이 입력되었을 때, n번째 피보나치 수를 1234567으로 나눈 나머지를 리턴하는 함수, solution을 완성해 주세요.
+
+제한 사항
+n은 2 이상 100,000 이하인 자연수입니다.
+입출력 예
+n	return
+3	2
+5	5
+입출력 예 설명
+피보나치수는 0번째부터 0, 1, 1, 2, 3, 5, ... 와 같이 이어집니다.
+
+
+ */
