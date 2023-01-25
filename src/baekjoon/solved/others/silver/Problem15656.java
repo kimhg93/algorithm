dd69d7e59656adf169ef7a1029e337d614bc6670
kimhg93 <kimhg93@gmail.com>
2023-01-25 17:45:59 +0900

    baekjoon N과 M

---
+++ src/baekjoon/solved/others/silver/Problem15656.java
@@ -0,0 +1,117 @@
+package baekjoon.solved.others.silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Problem15656 {
+    private static final BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+    private static final StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        StringTokenizer st = new StringTokenizer(r.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        st = new StringTokenizer(r.readLine());
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        combi(arr, new int[m], 0);
+        System.out.println(sb.toString().replaceAll("[\\[\\],]", ""));
+    }
+
+    static void combi(int[] arr, int[] re, int d){
+        if(d == re.length){
+            sb.append(Arrays.toString(re)).append("\n");
+            return;
+        }
+        for(int i=0; i<arr.length; i++){
+            re[d] = arr[i];
+            combi(arr, re, d+1);
+        }
+    }
+}
+
+
+/*
+N과 M (7)
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+1 초	512 MB	14677	11535	9399	79.189%
+문제
+N개의 자연수와 자연수 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오. N개의 자연수는 모두 다른 수이다.
+
+N개의 자연수 중에서 M개를 고른 수열
+같은 수를 여러 번 골라도 된다.
+입력
+첫째 줄에 N과 M이 주어진다. (1 ≤ M ≤ N ≤ 7)
+
+둘째 줄에 N개의 수가 주어진다. 입력으로 주어지는 수는 10,000보다 작거나 같은 자연수이다.
+
+출력
+한 줄에 하나씩 문제의 조건을 만족하는 수열을 출력한다. 중복되는 수열을 여러 번 출력하면 안되며, 각 수열은 공백으로 구분해서 출력해야 한다.
+
+수열은 사전 순으로 증가하는 순서로 출력해야 한다.
+
+예제 입력 1
+3 1
+4 5 2
+예제 출력 1
+2
+4
+5
+예제 입력 2
+4 2
+9 8 7 1
+예제 출력 2
+1 1
+1 7
+1 8
+1 9
+7 1
+7 7
+7 8
+7 9
+8 1
+8 7
+8 8
+8 9
+9 1
+9 7
+9 8
+9 9
+예제 입력 3
+3 3
+1231 1232 1233
+예제 출력 3
+1231 1231 1231
+1231 1231 1232
+1231 1231 1233
+1231 1232 1231
+1231 1232 1232
+1231 1232 1233
+1231 1233 1231
+1231 1233 1232
+1231 1233 1233
+1232 1231 1231
+1232 1231 1232
+1232 1231 1233
+1232 1232 1231
+1232 1232 1232
+1232 1232 1233
+1232 1233 1231
+1232 1233 1232
+1232 1233 1233
+1233 1231 1231
+1233 1231 1232
+1233 1231 1233
+1233 1232 1231
+1233 1232 1232
+1233 1232 1233
+1233 1233 1231
+1233 1233 1232
+1233 1233 1233
+ */
